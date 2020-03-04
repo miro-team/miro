a071faba3665b43ffc8584ae268e976b38cd6423
Михайлов Илья Николаевич <inmikhaylov@phoenixit.ru>
2020-03-04 14:05:04 +0300

    MIRO-47 Fix /api/user

--- src/main/java/miet/rooms/security/web/exception/UserNotFoundException.java
+++ src/main/java/miet/rooms/security/web/exception/UserNotFoundException.java
@@ -3,6 +3,6 @@ package miet.rooms.security.web.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "User not found")
+@ResponseStatus(code = HttpStatus.UNAUTHORIZED, reason = "User not found")
 public class UserNotFoundException extends Exception {
 }
