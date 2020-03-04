e4ca6de29a77a253c5bce9bb12018e1c637ec91c
Михайлов Илья Николаевич <inmikhaylov@phoenixit.ru>
2020-03-04 13:35:37 +0300

    MIRO-48 Add cors configuration

---
+++ src/main/java/miet/rooms/api/config/CorsConfiguration.java
@@ -0,0 +1,16 @@
+package miet.rooms.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Profile("api")
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
+    }
+}
